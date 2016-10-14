3cd258e2d59de5ca4e6f7c1a0959c907b552a269
snimbalk <snimbalk@hawk.iit.edu>
2016-10-14 22:46:49 +0000

    Used positional parameter for destroy-env.sh to identify specific autoscaling group

--- destroy-env.sh
+++ destroy-env.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 #Example of shell script execution
-#sh destroy-env.sh
+#sh destroy-env.sh snimbalk-auto-scaling-group
 
 #******************************************************************************************
+autoscaling_grp_name=$1
+#autoscaling_grp_name=`aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].AutoScalingGroupName'`
 
-load_balancer_name=`aws elb describe-load-balancers --query 'LoadBalancerDescriptions[*].LoadBalancerName'`
-launch_config_name=`aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].LaunchConfigurationName'`
-autoscaling_grp_name=`aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].AutoScalingGroupName'`
+load_balancer_name=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $autoscaling_grp_name --query 'AutoScalingGroups[*].LoadBalancerNames'`
+launch_config_name=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $autoscaling_grp_name --query 'AutoScalingGroups[*].LaunchConfigurationName'`
+
+#load_balancer_name=`aws elb describe-load-balancers --query 'LoadBalancerDescriptions[*].LoadBalancerName'`
+#launch_config_name=`aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].LaunchConfigurationName'`
+if [ $# -ne 1 ] ; then
+	echo  " Please provide correct number of arguments..Expected argument count is 1."
+	echo "e.g : sh destroy-env.sh snimbalk-auto-scaling-group"
+
+else
 
 #Detach load balancer from autoscaling goup
 aws autoscaling detach-load-balancers --load-balancer-names $load_balancer_name --auto-scaling-group-name $autoscaling_grp_name
@@ -37,3 +46,5 @@ aws elb delete-load-balancer --load-balancer-name $load_balancer_name
 echo "Load balancer is deleted..."
 
 echo "Process completed..."
+
+fi
