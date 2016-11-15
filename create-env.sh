b56b8eace2be42ca4dc2226aae07dbe3ffd71ee0
snimbalk <snimbalk@hawk.iit.edu>
2016-11-15 00:42:17 +0000

    Modified create and destroy files

--- create-env.sh
+++ create-env.sh
@@ -1,54 +1,40 @@
 #!/bin/bash
 #Execution of shell script
-#sh create-env.sh ami-438b2a23 snimbalk sg-fd8c4384 3 snimbalk-load-balancer snimbalk-launch-config snimbalk-auto-scaling-group firsttoken
-#*****************************************************************************************************************************
+#sh create-env.sh ami-438b2a23 snimbalk sg-fd8c4384 snimbalk-load-balancer snimbalk-launch-config snimbalk-auto-scaling-group 2
+#********************************************************************************************************************
 
 #Variable declaration
 image_id=$1
 key_name=$2
 security_grp_id=$3
-number_of_instances=$4
-client_token=$8
 availability_zones="us-west-2b"
-load_balancer_name=$5
-launch_config_name=$6
-autoscaling_grp_name=$7
-min_size=2
-max_size=5
-desired_size=4
+load_balancer_name=$4
+launch_config_name=$5
+autoscaling_grp_name=$6
+min_size=1
+max_size=2
+desired_size=$7
+#client_token=$8
 
-if [ $# -ne 8 ] ; then
-echo  " Please provide correct number of arguments..Expected argument count is 8."
-echo "e.g : sh create-env.sh ami-06b94666 snimbalk sg-fd8c4384 3 snimbalk-load-balancer snimbalk-launch-config snimbalk-auto-scaling-group xxx"
+if [ $# -ne 7 ] ; then
+	echo  -e "\n Please provide correct number of arguments..Expected argument count is 7."
+	echo -e "e.g : sh create-env.sh ami-06b94666 snimbalk sg-fd8c4384 snimbalk-load-balancer snimbalk-launch-config snimbalk-auto-scaling-group 2 \n"
 else
-echo " $@ "
+	#Create load balancer
+	aws elb create-load-balancer --load-balancer-name $load_balancer_name --security-groups $security_grp_id --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones $availability_zones
+	echo -e "\n Load balancer $load_balancer_name is created... \n"
 
-#Create instances
-aws ec2 run-instances --image-id $image_id --key-name $key_name  --security-group-ids $security_grp_id --instance-type t2.micro --count $number_of_instances --placement AvailabilityZone=$availability_zones --client-token $client_token --user-data file://installapp.sh --iam-instance-profile Name=developer
+	#Launch configuration for autoscaling	
+	aws autoscaling create-launch-configuration --launch-configuration-name $launch_config_name --image-id $image_id --key-name $key_name --security-groups $security_grp_id --instance-type t2.micro --user-data file://installapp.sh --iam-instance-profile developer
 
-#Wait till instances start running
-aws ec2 wait instance-running --filters "Name=client-token,Values=$client_token"
-echo "Instances are running...."
+	#Create autoscaling group
+	aws autoscaling create-auto-scaling-group --auto-scaling-group-name $autoscaling_grp_name --launch-configuration-name $launch_config_name --load-balancer-names $load_balancer_name --availability-zones $availability_zones --min-size $min_size --max-size $max_size --desired-capacity $desired_size
+	echo -e "Autoscaling group $autoscaling_grp_name is created.... \n"
 
-#Create load balancer
-aws elb create-load-balancer --load-balancer-name $load_balancer_name --security-groups $security_grp_id --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones $availability_zones
-echo "Load balancer $5 is created..."
-
-#Attach instances to load balancer
-ID=`aws ec2 describe-instances --filters "Name=client-token,Values=$client_token" --query 'Reservations[*].Instances[].InstanceId'`
-aws elb register-instances-with-load-balancer --load-balancer-name $load_balancer_name --instances $ID
-
-#Launch configuration for autoscaling
-aws autoscaling create-launch-configuration --launch-configuration-name $launch_config_name --image-id $image_id --key-name $key_name --security-groups $security_grp_id --instance-type t2.micro --user-data file://installapp.sh --iam-instance-profile developer
-
-#Create autoscaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name $autoscaling_grp_name --launch-configuration-name $launch_config_name --load-balancer-names $load_balancer_name --availability-zones $availability_zones --min-size 0 --max-size $max_size --desired-capacity 0
-echo "Autoscaling group $7 is created.."
-
-aws autoscaling attach-instances --instance-ids $ID --auto-scaling-group-name $autoscaling_grp_name
-
-aws autoscaling update-auto-scaling-group --auto-scaling-group-name $autoscaling_grp_name --launch-configuration-name $launch_config_name --min-size $min_size --desired-capacity $desired_size	
-
-echo "Process completed...."
-
+	#Wait till all instances start running
+        aws ec2 wait instance-running --filters "Name=image-id,Values=$image_id" "Name=instance-state-code,Values=0,16"
+	echo "Below instances are running...."
+	ID=`aws ec2 describe-instances --filters "Name=image-id,Values=$image_id" "Name=instance-state-code,Values=16" --query 'Reservations[*].Instances[].InstanceId'`
+	echo $ID
+	echo -e "\n Process is completed...."
 fi
--- destroy-env.sh
+++ destroy-env.sh
@@ -1,54 +1,46 @@
 #!/bin/bash
-#Example of shell script execution
-#sh destroy-env.sh snimbalk-auto-scaling-group
+#Execution of shell script
+#sh destroy-env.sh
 
 #******************************************************************************************
-autoscaling_grp_name=$1
-autoscaling_details=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $1`
-#autoscaling_grp_name=`aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].AutoScalingGroupName'`
 
-load_balancer_name=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $autoscaling_grp_name --query 'AutoScalingGroups[*].LoadBalancerNames'`
-launch_config_name=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $autoscaling_grp_name --query 'AutoScalingGroups[*].LaunchConfigurationName'`
+load_balancer_name=`aws elb describe-load-balancers --query 'LoadBalancerDescriptions[*].LoadBalancerName'`
+launch_config_name=`aws autoscaling describe-launch-configurations --query 'LaunchConfigurations[*].LaunchConfigurationName'`
+autoscaling_grp_name=`aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].AutoScalingGroupName'`
 
-#load_balancer_name=`aws elb describe-load-balancers --query 'LoadBalancerDescriptions[*].LoadBalancerName'`
-#launch_config_name=`aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].LaunchConfigurationName'`
-if [ $# -ne 1 ] ; then
-	echo  " Please provide correct number of arguments..Expected argument count is 1."
-	echo "e.g : sh destroy-env.sh snimbalk-auto-scaling-group"
+#Delete Autoscaling Groups
+echo -e "\n List of autoscaling groups: \n $autoscaling_grp_name \n"
+for i in $autoscaling_grp_name
+do
+	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $i --min-size 0 --desired-capacity 0
+	aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $i --force-delete  
+	echo "Autoscaling group $i is deleted..."
+	sleep 5
+done
 
-else
+#Delete launch configurations
+echo -e "\n List of launch configurations: \n $launch_config_name \n"
+for i in $launch_config_name
+do
+	aws autoscaling delete-launch-configuration --launch-configuration-name $i
+	echo "Launch configuration $i is deleted..."
+	sleep 5
+done
 
-        if [ "$autoscaling_details" = "" ];
-        then
-			echo "Please provide correct autoscaling group name. $1 autoscaling group doesn't exists."
-	else
-		#Detach load balancer from autoscaling goup
-		aws autoscaling detach-load-balancers --load-balancer-names $load_balancer_name --auto-scaling-group-name $autoscaling_grp_name
-		echo "Load balancer detached..."
+#Delete load balancers
+echo -e "\n List of load balancers: \n $load_balancer_name \n"
+for i in $load_balancer_name
+do
+	aws elb delete-load-balancer --load-balancer-name $i
+	echo "Load balancer $i is deleted..."
+  	sleep 1
+done
 
-		#Set desired capacity to 0
-		aws autoscaling update-auto-scaling-group --auto-scaling-group-name $autoscaling_grp_name --launch-configuration-name $launch_config_name --min-size 0 --desired-capacity 0
+#Terminate instances
+all_instances=`aws ec2 describe-instances --query 'Reservations[*].Instances[].InstanceId'`
+aws ec2 terminate-instances --instance-ids $all_instances
 
-		#Wait to terminate instances
-		all_instances=`aws ec2 describe-instances --query 'Reservations[*].Instances[].InstanceId'`
-		aws ec2 wait instance-terminated --instance-ids $all_instances
-		echo "All instances are terminated..."
+aws ec2 wait instance-terminated --instance-ids $all_instances
+echo -e "\n All instances are terminated... \n"
 
-		#Delete autoscaling group
-		aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $autoscaling_grp_name --force-delete  
-		echo "Autoscaling group is deleted..."
-
-		#Delete launch configurations
-		aws autoscaling delete-launch-configuration --launch-configuration-name $launch_config_name
-		echo "Lauch configurations are deleted..."
-
-		#Delete listeners
-		aws elb delete-load-balancer-listeners --load-balancer-name $load_balancer_name --load-balancer-ports 80
-
-		#Delete load-balancer
-		aws elb delete-load-balancer --load-balancer-name $load_balancer_name
-		echo "Load balancer is deleted..."
-
-		echo "Process completed..."
-	fi
-fi
+echo "Process is completed..."
