ed846695196093441c8c496bd57584532e4f6d68
snimbalk <snimbalk@hawk.iit.edu>
2016-11-28 13:03:31 -0800

    Add files via upload

--- final_project/create-env.sh
+++ final_project/create-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Execution of shell script
-#sh create-env.sh ami-438b2a23 snimbalk sg-fd8c4384 snimbalk-load-balancer snimbalk-launch-config snimbalk-auto-scaling-group 2
+#sh create-env.sh ami-438b2a23 snimbalk sg-fd8c4384 snimbalk-load-balancer snimbalk-launch-config snimbalk-auto-scaling-group 4
 #********************************************************************************************************************
 
 #Variable declaration
@@ -11,15 +11,15 @@ availability_zones="us-west-2b"
 load_balancer_name=$4
 launch_config_name=$5
 autoscaling_grp_name=$6
-min_size=1
-max_size=2
+min_size=2
+max_size=5
 desired_size=$7
 #client_token=$8
 
 if [ $# -ne 7 ] ; then
 	echo  -e "\n Please provide correct number of arguments..Expected argument count is 7."
 	echo -e "e.g : sh create-env.sh ami-06b94666 snimbalk sg-fd8c4384 snimbalk-load-balancer snimbalk-launch-config snimbalk-auto-scaling-group 2 \n"
-else
+else	
 	#Create load balancer
 	aws elb create-load-balancer --load-balancer-name $load_balancer_name --security-groups $security_grp_id --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones $availability_zones
 	echo -e "\n Load balancer $load_balancer_name is created... \n"
@@ -32,8 +32,15 @@ else
 	echo -e "Autoscaling group $autoscaling_grp_name is created.... \n"
 
 	#Wait till all instances start running
-        aws ec2 wait instance-running --filters "Name=image-id,Values=$image_id" "Name=instance-state-code,Values=0,16"
+    aws ec2 wait instance-running --filters "Name=image-id,Values=$image_id" "Name=instance-state-code,Values=0,16"
+		
+	#Create instance to execute edit.php
+	aws ec2 run-instances --image-id $image_id --key-name $key_name  --security-group-ids $security_grp_id --instance-type t2.micro --count 1 --placement AvailabilityZone=$availability_zones --user-data file://installappcronjob.sh --iam-instance-profile Name=developer
+
+	#Wait till instances start running
+	aws ec2 wait instance-running --filters "Name=image-id,Values=$image_id" "Name=instance-state-code,Values=0,16"
 	echo "Below instances are running...."
+	
 	ID=`aws ec2 describe-instances --filters "Name=image-id,Values=$image_id" "Name=instance-state-code,Values=16" --query 'Reservations[*].Instances[].InstanceId'`
 	echo $ID
 	echo -e "\n Process is completed...."
