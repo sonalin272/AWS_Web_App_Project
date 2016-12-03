e721791a05a4d099f172b2af3aaea3fd66fe88f7
snimbalk <snimbalk@hawk.iit.edu>
2016-12-03 08:18:36 +0000

    Added cookie policy

--- create-env.sh
+++ create-env.sh
@@ -24,6 +24,16 @@ else
 	aws elb create-load-balancer --load-balancer-name $load_balancer_name --security-groups $security_grp_id --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones $availability_zones
 	echo "\n Load balancer $load_balancer_name is created... \n"
 
+	#Configure the elb configure-health-check and attach cookie stickiness policy
+
+	echo  "Configuring health and cookie stickiness policies for load balancer \n"
+
+	aws elb configure-health-check --load-balancer-name $load_balancer_name --health-check Target=HTTP:80/index.php,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+
+	aws elb create-lb-cookie-stickiness-policy --load-balancer-name $load_balancer_name --policy-name snimbalk-lb-cookie-policy --cookie-expiration-period 60
+
+	aws elb set-load-balancer-policies-of-listener --load-balancer-name $load_balancer_name --load-balancer-port 80 --policy-names snimbalk-lb-cookie-policy
+
 	#Launch configuration for autoscaling	
 	aws autoscaling create-launch-configuration --launch-configuration-name $launch_config_name --image-id $image_id --key-name $key_name --security-groups $security_grp_id --instance-type t2.micro --user-data file://installapp.sh --iam-instance-profile developer
 
@@ -35,8 +45,9 @@ else
         aws ec2 wait instance-running --filters "Name=image-id,Values=$image_id" "Name=instance-state-code,Values=0,16"
 		
 	#Create instance to execute edit.php
+	echo "Cron job will be scheduled on below instance.. \n"
 	aws ec2 run-instances --image-id $image_id --key-name $key_name  --security-group-ids $security_grp_id --instance-type t2.micro --count 1 --placement AvailabilityZone=$availability_zones --user-data file://installappcronjob.sh --iam-instance-profile Name=developer
-
+	
 	#Wait till instances start running
 	aws ec2 wait instance-running --filters "Name=image-id,Values=$image_id" "Name=instance-state-code,Values=0,16"
 	echo "Below instances are running...."	
